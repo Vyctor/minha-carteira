4d8db63f46dee4d38932e914f7fa659428f11c95
Vyctor <dev.vyctor@gmail.com>
2020-11-25 21:29:01 -0300

    feat: criacao dos componentes de Layout

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Layout from "../../components/Layout";
 
 const Dashboard: React.FC = () => {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <Layout />
     </div>
   );
 };
