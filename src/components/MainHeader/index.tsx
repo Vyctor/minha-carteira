2dec5102b07966baa5bd763eff633a328fdd0928
Vyctor <dev.vyctor@gmail.com>
2021-03-05 20:54:17 -0300

    feat: criando o header principal

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useMemo } from "react";
+import emojis from "../../utils/emojis";
 
-import { Container } from "./styles";
+import { Container, Profile, Welcome, Username } from "./styles";
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    return emojis[Math.floor(Math.random() * emojis.length)];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <h1>Toggle</h1>
+      <Profile>
+        <Welcome>Olá, {emoji} </Welcome>
+        <Username>Vyctor Guimarães</Username>
+      </Profile>
     </Container>
   );
 };
