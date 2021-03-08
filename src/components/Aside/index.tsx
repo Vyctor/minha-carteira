0883836bd22babe61abac6e6049043193051b171
Vyctor <dev.vyctor@gmail.com>
2021-03-07 21:42:28 -0300

    feat: criando menu lateral

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from "react-icons/md";
 
-import { Container } from "./styles";
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Header, LogoImg, MenuContainer, MenuItemLink, Title } from "./styles";
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logoImg} alt="Logo Minha Carteira" />
+        <Title>Minha Carteira</Title>
+      </Header>
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard></MdDashboard>
+          Dashboard
+        </MenuItemLink><MenuItemLink href="#">
+          <MdArrowUpward></MdArrowUpward>
+          Entradas
+        </MenuItemLink><MenuItemLink href="#">
+          <MdArrowDownward></MdArrowDownward>
+          Saídas
+        </MenuItemLink><MenuItemLink href="#">
+          <MdExitToApp></MdExitToApp>
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
